7aecf1d91208c9c05ae1b380b9930ca49d50e786
David Edwards <david@davwards.com>
2018-11-23 11:06:50 -0500

    Wire up CLI with real dependencies

--- packages/applications/cli/bin/todopia.ts
+++ packages/applications/cli/bin/todopia.ts
@@ -1,15 +1,62 @@
 import { existsSync, mkdirSync } from 'fs'
 import { join } from 'path'
+import * as readline from 'readline'
 
 import { FsBackedRepository } from '@todopia/filesystem-persistence-plugin'
+import { FsBackedSession } from '../session/fs-backed-session'
 
-const todoHome = join(process.env.HOME, '.todopia')
+import {
+  createTask,
+  completeTask
+} from '@todopia/tasks-core'
 
-if(!existsSync(todoHome)) mkdirSync(todoHome)
+import {
+  createPlayer
+} from '@todopia/players-core'
 
-const persistence = FsBackedRepository(todoHome)
+import {
+  awardPlayerWithCurrency
+} from '@todopia/tasks-player-currency-plugin'
+
+import { Cli } from '..'
+
+const todopiaHome = join(process.env.HOME, '.todopia')
+
+if(!existsSync(todopiaHome)) mkdirSync(todopiaHome)
+
+const persistence = FsBackedRepository(todopiaHome)
+const session = FsBackedSession(todopiaHome)
 
 const args = process.argv.slice(2)
 
-console.log(args)
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
+Cli({
+  session,
+  createPlayer: createPlayer(persistence, persistence),
+  createTask: createTask(persistence),
+  completeTask: completeTask(
+    persistence,
+    awardPlayerWithCurrency(persistence)
+  ),
+  taskRepository: persistence,
+  playerRepository: persistence,
+  ui: {
+    choice: (prompt: string, choices: string[]) => {
+      console.log('')
+      console.log(prompt)
+      choices.forEach((choice, i) => {
+        console.log(`  ${i}: ${choice}`)
+      })
+
+      return new Promise((resolve) => {
+        rl.question('Enter the number of your choice: ', answer => {
+          resolve(answer)
+        })
+      })
+    }
+  },
+})(args).then(() => { rl.close() })
